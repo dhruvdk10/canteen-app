64f54fc7469b08dd006a40917ea81bd911f662c1
Dhruv Kapoor <dhruvkapoor200210@gmail.com>
2026-04-03 21:03:16 +0530

    'lu'

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
 
           {/* Logo & Badge */}
           <div className="d-flex align-items-center mb-3 mb-md-0">
-            <img src="/canteen-app/edzy-logo.svg" alt="Edzy Logo" style={{ height: "40px" }} />
+            <img src="/edzy-logo.svg" alt="Edzy Logo" style={{ height: "40px" }} />
             <span className="food-badge d-flex align-items-center justify-content-center ms-2">
               <FontAwesomeIcon icon={faUtensils} />
             </span>
