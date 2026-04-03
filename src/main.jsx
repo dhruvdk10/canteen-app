a9f312b3f77aa893b2724114660b1ebeda73cbea
Dhruv Kapoor <dhruvkapoor200210@gmail.com>
2026-04-03 19:12:25 +0530

    'lu'

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
@@ -8,8 +8,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/canteen-app/">
+    <HashRouter basename="/canteen-app/">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
