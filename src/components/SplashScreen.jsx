dfe6df16b5cc6513c020d2d8e2555a4bba6a17f6
Dhruv Kapoor <dhruvkapoor200210@gmail.com>
2026-04-03 05:55:15 +0530

    'lu'

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -20,7 +20,7 @@ const SplashScreen = ({ onFinish }) => {
           alt="Edzy Logo"
           className="splash-logo-img"
         />
-        <div className="ms-2 d-flex align-items-center">
+        <div className="ms-3 d-flex align-items-center">
           <span className="food-badge splash-badge d-flex align-items-center justify-content-center me-1">
             <FontAwesomeIcon icon={faUtensils} />
           </span>
