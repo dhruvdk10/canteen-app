6fa29749c7085415bcd8317431aea5699917ed18
Dhruv Kapoor <dhruvkapoor200210@gmail.com>
2026-04-03 18:09:04 +0530

    'lu'

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isRender = process.env.RENDER || false
+const basePath = isRender ? '/' : '/canteen-app/'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/canteen-app/'
+  base: basePath,
 })
