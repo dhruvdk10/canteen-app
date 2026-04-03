7b3d231da2e34ecc321d6982e7ea83769ada788e
Dhruv Kapoor <dhruvkapoor200210@gmail.com>
2026-04-03 18:26:40 +0530

    'lu'

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "./index.css";
 
-const isRender = import.meta.env.VITE_RENDER === 'true'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={isRender ? '/' : '/canteen-app/'}>
+    <BrowserRouter basename="/canteen-app/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
-)
+);
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const isRender = process.env.RENDER || false
-const basePath = isRender ? '/' : '/canteen-app/'
-
 export default defineConfig({
   plugins: [react()],
-  base: basePath,
+  base: '/canteen-app/'
 })
